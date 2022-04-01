f1d405e7466e1e7cc1aec6f532a45fc8e1cbf934
suyashrastogi7 <suyashrastogi@protonmail.com>
2022-04-01 19:52:31 +0530

    Updated Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["SF Pro Display"],
+      },
+    },
   },
   plugins: [],
 };
